f0b8c189eb7ad67e829865f9a67be208b10d6d26
Lyon <lyondhill@gmail.com>
2017-08-18 09:12:15 -0600

    update some tests and remove a test file

--- main_test.go
+++ main_test.go
@@ -9,11 +9,13 @@ import (
 func TestStartServer(t *testing.T) {
 	go startServer(1234)
 
+	// other tests ensure the rest of the functions are working
+	// we just need to confirm server can start
 	conn, _ := net.Dial("tcp", "localhost:1234")
 	fmt.Fprintln(conn, "QUERY|hi|")
 	var result string
 	fmt.Fscanln(conn, &result)
-	if result != "fail" {
+	if result != "FAIL" {
 		t.Errorf("recieved invalid result: %s", result)
 	}
 	conn.Close()
